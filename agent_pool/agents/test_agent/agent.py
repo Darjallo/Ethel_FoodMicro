ff5fd82bf4741a9757713e329557f68549a90abf
Gerd Kortemeyer <kgerd@ethz.ch>
2025-05-22 18:06:12 +0200

    Simple Echo Agent

---
+++ agent_pool/agents/test_agent/agent.py
@@ -0,0 +1,38 @@
+# Project Ethel
+# Test Echo Agent
+#
+# Copyright (C) 2025  Gerd Kortemeyer, ETH Zurich
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
+
+from datetime import datetime
+from agent_pool.base_agent_server import run_server
+
+class TestAgent:
+    def handle(self, request_json):
+        # Non-streaming: Echo input, add "processed" timestamp
+        response = dict(request_json)
+        response["processed"] = datetime.now().isoformat()
+        return response
+
+    def stream(self, request_json):
+        # Streaming: Just yield "Processed at [datetime]"
+        msg = f"Processed at {datetime.now().isoformat()}"
+        yield {"message": msg}
+
+if __name__ == "__main__":
+    run_server(port=8000, handler_instance=TestAgent())
+
