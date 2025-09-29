f2c0de23ac8f215225306a6037a509d94ebbd00b
Emir Isman <eisman@ethz.ch>
2025-09-29 17:48:32 +0200

    bump langgraph dependencies

--- ethelflow/handler.py
+++ ethelflow/handler.py
@@ -3,37 +3,35 @@ from fastapi.responses import StreamingResponse
 
 
 async def handler(
-    mod: ModuleType, context: dict, query: dict, file_id: str, stream: bool
+    mod: ModuleType,
+    context: dict,
+    query: dict,
+    file_id: str,
+    stream: bool,
+    checkpointer=None,
 ):
     if stream:
 
         async def stream():
             async for update in mod.run(
-                context=context, query=query, file_id=file_id, stream=True
+                context=context,
+                query=query,
+                file_id=file_id,
+                stream=True,
+                checkpointer=checkpointer,
             ):
                 yield update
 
         return StreamingResponse(stream(), media_type="application/json")
     else:
-        gen = mod.run(context=context, query=query, file_id=file_id, stream=False)
+        gen = mod.run(
+            context=context,
+            query=query,
+            file_id=file_id,
+            stream=False,
+            checkpointer=checkpointer,
+        )
 
         first = await anext(gen, {})
 
         return first
-
-
-def handler_stream(
-    mod: ModuleType, context: dict, query: dict, file_id: str, flow_name: str
-) -> StreamingResponse:
-    """
-    Handle streaming response for flow execution.
-    """
-
-    def stream():
-        # run_id = str(uuid.uuid4())
-        for update in mod.run(
-            context=context, query=query, file_id=file_id, stream=True
-        ):
-            yield f"{update}\n"
-
-    return StreamingResponse(stream(), media_type="application/json")
